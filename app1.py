2f922fabd3a41ecc5666781be0ac189b241393f6
Yoselin Alvarez <yvalvarezl@gmail.com>
2026-08-27 08:01:22 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,9 +5,9 @@ import pytesseract
 from PIL import Image
 
 
-st.title("Reconocimiento óptico de Caracteres")
+st.title("Mi OCR")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.camera_input("Toma una foto")
 
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
